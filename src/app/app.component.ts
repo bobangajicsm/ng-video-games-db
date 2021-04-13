d81e466678899bde8329898c70c3b1fbe5c20cf6
Slobodan Gajic <slobodan.gajic@multiloginapp.com>
2021-04-13 19:48:17 +0200

    Added video game fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-video-games-db';
+
+  ngOnInit() {
+    fetch("https://rawg-video-games-database.p.rapidapi.com/games", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H",
+		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+  }
 }
